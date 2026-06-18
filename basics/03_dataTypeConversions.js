52b412a7228a4dbeb409b2d98836581b37796cce
shradha224 <shradha224shrivastava@gmail.com>
2026-06-18 15:32:59 +0000

    type conversions

---
+++ basics/03_dataTypeConversions.js
@@ -0,0 +1,15 @@
+let num="33"
+console.log(typeof num)
+let marks=Number(num)
+console.log(marks)
+/*
+33abc=> NaN (Not a number)
+null=>0
+true=>1 , false=>0
+
+---TYPE CONVERSIONS---
+
+Number()
+String()
+Boolean()
+*/
